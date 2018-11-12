5d693c29d07e710e9e59bb873df64910300d886d
AngelGus79 <agustavo79@gmail.com>
2018-11-12 14:28:42 -0600

    Manage Object Life Cycle project initialized

---
+++ CSharp/ManageObjectLifeCycle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManageObjectLifeCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
